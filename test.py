9e20aee391eca7f373c310dfa5543aa16c0f4650
shawnk3 <shawnekuriakose@gmail.com>
2022-02-23 03:16:46 +0000

    second commit

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 from random import randint
 from time import sleep
 from buildhat import Motor
+
+new_angle = randint(-180,180)
+print(new_angle)
+
+motor_y = Motor('A')
+motor_y.run_to_position(90, 100)
